e36846d514e50f95a4bc0ee54ed7a372df505c24
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-05-18 18:23:24 +0300

    project deploy

---
+++ app.py
@@ -0,0 +1,45 @@
+import SessionState
+import streamlit as st
+import tensorflow as tf
+
+### Streamlit code (works as a straigtht-forward script) ###
+st.title("Diagnostic Classification of Cervical Cell Images")
+@st.cache # cache the function so predictions aren't always redone (Streamlit refreshes every click)
+def make_prediction(image, model, class_names):
+
+    image = tf.cast(tf.expand_dims(image, axis=0), tf.float32)
+    
+    pred_class = class_names[tf.argmax(preds[0])]
+    pred_conf = tf.reduce_max(preds[0])
+    return image, pred_class, pred_conf
+
+# Display info about model and classes
+if st.checkbox("Show classes"):
+    st.write(f"You chose {MODEL}, these are the classes of food it can identify:\n", CLASSES)
+
+# File uploader allows user to add their own image
+uploaded_file = st.file_uploader(label="Upload an image",
+                                 type=["bmp"])
+
+# Setup session state to remember state of app so refresh isn't always needed
+# See: https://discuss.streamlit.io/t/the-button-inside-a-button-seems-to-reset-the-whole-app-why/1051/11 
+session_state = SessionState.get(pred_button=False)
+
+# Create logic for app flow
+if not uploaded_file:
+    st.warning("Please upload an image.")
+    st.stop()
+else:
+    session_state.uploaded_image = uploaded_file.read()
+    st.image(session_state.uploaded_image, use_column_width=True)
+    pred_button = st.button("Predict")
+
+# Did the user press the predict button?
+if pred_button:
+    session_state.pred_button = True 
+
+# And if they did...
+if session_state.pred_button:
+    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
+    st.write(f"Prediction: {session_state.pred_class}, \
+               Confidence: {session_state.pred_conf:.3f}")
