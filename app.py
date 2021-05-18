67cbbeb63e26752402fef85af96750aecebbdf5e
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-05-18 18:34:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import SessionState
 import streamlit as st
 import tensorflow as tf
+from keras.optimizers import Adam
+from keras.models import load_model
+
+model = load_model('MobileNet.h5')
+model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+
+class_names = [
+    'Dyskeratotic',
+    'Koilocytotic',
+    'Metaplastic',
+    'Parabasal',
+    'Superficial-Intermediate']
 
 ### Streamlit code (works as a straigtht-forward script) ###
 st.title("Diagnostic Classification of Cervical Cell Images")
 @st.cache # cache the function so predictions aren't always redone (Streamlit refreshes every click)
 def make_prediction(image, model, class_names):
 
-    image = tf.cast(tf.expand_dims(image, axis=0), tf.float32)
-    
+    img = tf.image.decode_bmp(img, channels=3)
+    img = tf.image.resize(img,[224,224])
+    img /= 255.
+    img = tf.cast(tf.expand_dims(img, axis=0), tf.float32)
+
+    preds = model.predict(img)
     pred_class = class_names[tf.argmax(preds[0])]
     pred_conf = tf.reduce_max(preds[0])
     return image, pred_class, pred_conf
@@ -21,8 +37,7 @@ if st.checkbox("Show classes"):
 uploaded_file = st.file_uploader(label="Upload an image",
                                  type=["bmp"])
 
-# Setup session state to remember state of app so refresh isn't always needed
-# See: https://discuss.streamlit.io/t/the-button-inside-a-button-seems-to-reset-the-whole-app-why/1051/11 
+#buttuon to trigger prediction
 session_state = SessionState.get(pred_button=False)
 
 # Create logic for app flow
@@ -38,7 +53,6 @@ else:
 if pred_button:
     session_state.pred_button = True 
 
-# And if they did...
 if session_state.pred_button:
     session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
     st.write(f"Prediction: {session_state.pred_class}, \
