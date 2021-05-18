bd38a5da0ec774d9e2b5f952199dac82b5215029
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-05-18 18:48:52 +0300

    update

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import SessionState
 import streamlit as st
 import tensorflow as tf
-from keras.optimizers import Adam
-from keras.models import load_model
 
-model = load_model('MobileNet.h5')
-model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+model = tf.keras.models.load_model('MobileNet.h5')
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 class_names = [
     'Dyskeratotic',
@@ -17,9 +15,9 @@ class_names = [
 ### Streamlit code (works as a straigtht-forward script) ###
 st.title("Diagnostic Classification of Cervical Cell Images")
 @st.cache # cache the function so predictions aren't always redone (Streamlit refreshes every click)
-def make_prediction(image, model, class_names):
+def make_prediction(image):
 
-    img = tf.image.decode_bmp(img, channels=3)
+    img = tf.image.decode_bmp(image, channels=3)
     img = tf.image.resize(img,[224,224])
     img /= 255.
     img = tf.cast(tf.expand_dims(img, axis=0), tf.float32)
@@ -28,10 +26,6 @@ def make_prediction(image, model, class_names):
     pred_class = class_names[tf.argmax(preds[0])]
     pred_conf = tf.reduce_max(preds[0])
     return image, pred_class, pred_conf
-
-# Display info about model and classes
-if st.checkbox("Show classes"):
-    st.write(f"You chose {MODEL}, these are the classes of food it can identify:\n", CLASSES)
 
 # File uploader allows user to add their own image
 uploaded_file = st.file_uploader(label="Upload an image",
@@ -54,6 +48,6 @@ if pred_button:
     session_state.pred_button = True 
 
 if session_state.pred_button:
-    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
+    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image)
     st.write(f"Prediction: {session_state.pred_class}, \
                Confidence: {session_state.pred_conf:.3f}")
